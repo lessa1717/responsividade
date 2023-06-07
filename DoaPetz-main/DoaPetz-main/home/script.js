7b6be8f552a5912e87683584c5b0a7411cc2df95
oliverlucas983@gmail.com <04517840573@info.local>
2023-06-06 21:53:59 -0300

    aula06-06

---
+++ DoaPetz-main/DoaPetz-main/home/script.js
@@ -0,0 +1,16 @@
+let menu = document.getElementById("links-navegacao");
+let menu_barras = document.getElementById("menu_barras");
+
+
+
+function mostrarMenu() {
+    if (window.getComputedStyle(menu).display == "none") {
+        menu.style.display = "flex"
+        menu_barras.setAttribute("arial-label","fechar menu")
+        menu_barras.setAttribute("aria-expanded", true)
+        }else{
+            menu.style.display = "none"
+            menu_barras.setAttribute("arial-label","abrir menu")
+            menu_barras.setAttribute("aria-expanded", false)
+        }
+}
